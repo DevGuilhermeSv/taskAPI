8492c2634fed4b3cbc10ae44e2722da70e40bbe9
Guilherme Rodrigues <development.guilherme@gmail.com>
2023-11-05 16:28:32 -0300

    added auth.controller test

--- src/auth/Controller/auth.controller.spec.ts
+++ src/auth/Controller/auth.controller.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, HttpCode } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { AuthService } from '../Service/auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../../users/Service/users.service';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-    }).compile();
+      providers: [AuthService, UsersService, JwtService],
+    })
+      .useMocker((token) => {
+        if (token == 'UserRepository')
+          return { findOne: jest.fn((entity) => entity) };
+      })
+      .compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authController).toBeDefined();
+  });
+
+  describe('signIn', () => {
+    it('should sign in a user and return the result', async () => {
+      // Arrange
+      const signInDto = {
+        username: 'testuser',
+        password: 'testpassword',
+      };
+
+      const mockResult = { access_token: 'mockAccessToken' };
+
+      jest.spyOn(authService, 'signIn').mockResolvedValue(mockResult);
+
+      // Act
+      const result = await authController.signIn(signInDto);
+
+      // Assert
+      expect(result).toEqual(mockResult);
+    });
   });
 });
